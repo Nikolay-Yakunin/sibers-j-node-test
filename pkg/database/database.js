e195b9fec72dd08846552a3f89bec93ea1ac0291
Nikolay-Yakunin <akuninn52@gmail.com>
2025-07-12 21:34:59 +0700

    add comment about db driver to database.js

--- pkg/database/database.js
+++ pkg/database/database.js
@@ -2,6 +2,15 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
+
+// Install the appropriate database driver:
+// Docs: https://sequelize.org/docs/v6/getting-started/
+// npm install --save pg pg-hstore # Postgres
+// npm install --save mysql2
+// npm install --save mariadb
+// npm install --save sqlite3
+// npm install --save tedious # Microsoft SQL Server
+// npm install --save oracledb # Oracle Database
 const dbUrl = process.env.DB_URL || "postgres://user:pass@postgres:5432/admin_dashboard";
 
 
